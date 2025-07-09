33456db3fa43bae606686084478c464062c351eb
Rajdip-Sanyal <sanyalraj2@gmail.com>
2025-07-09 21:30:55 +0530

    Portfolio

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 // Typing Animation starts
-const texts = ["web developer", "problem solver"];
+const texts = ["a web developer", "a problem solver"];
 let index = 0;
 let charIndex = 0;
 let currentText = "";
